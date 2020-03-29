c348f411d6275ebfd8b729dad240161721c545d6
Martin Bentzen <martinbentzen@Martins-MacBook-Air.local>
2020-03-29 11:10:57 +0200

    redis

--- bin/casino-server.js
+++ bin/casino-server.js
@@ -11,6 +11,8 @@ var argv = require('minimist')(process.argv.slice(2));
 if(argv.p) conf.server.port = argv.p;
 if(argv.h) conf.server.host = argv.h;
 
+argv.v = 'redis://h:p4a0ccca5c529159eb6068ab5ea76bad8c04f67969ea86d3bc93c355d01abb34d@ec2-52-50-101-139.eu-west-1.compute.amazonaws.com:24249'
+
 if(argv.r) {
 	var words = argv.r.split(':');
 	if(words[0]) conf.redis.host = words[0];
