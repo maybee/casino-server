3749e7eb477ed7ec54676d687af801c60dc53e90
Martin Bentzen <martinbentzen@Martins-MacBook-Air.local>
2020-03-29 11:16:29 +0200

    redis

--- bin/casino-server.js
+++ bin/casino-server.js
@@ -11,13 +11,9 @@ var argv = require('minimist')(process.argv.slice(2));
 if(argv.p) conf.server.port = argv.p;
 if(argv.h) conf.server.host = argv.h;
 
-argv.v = 'redis://h:p4a0ccca5c529159eb6068ab5ea76bad8c04f67969ea86d3bc93c355d01abb34d@ec2-52-50-101-139.eu-west-1.compute.amazonaws.com:24249'
 
-if(argv.r) {
-	var words = argv.r.split(':');
-	if(words[0]) conf.redis.host = words[0];
-	if(words[1]) conf.redis.port = parseInt(words[1]);
-}
+conf.redis.host = 'h:p4a0ccca5c529159eb6068ab5ea76bad8c04f67969ea86d3bc93c355d01abb34d@ec2-52-50-101-139.eu-west-1.compute.amazonaws.com';
+conf.redis.port = 24249;
 
 var server = new Server().startup( conf );
 var casino = new Casino().startup( conf );
