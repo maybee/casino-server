83468b78486100d7170ccdc80cf20cade371c98c
Martin Bentzen <martinbentzen@Martins-MacBook-Air.local>
2020-03-29 17:22:09 +0200

    ok

--- bin/casino-server.js
+++ bin/casino-server.js
@@ -12,8 +12,8 @@ if(argv.p) conf.server.port = argv.p;
 if(argv.h) conf.server.host = argv.h;
 
 
-conf.redis.host = 'h:p4a0ccca5c529159eb6068ab5ea76bad8c04f67969ea86d3bc93c355d01abb34d@ec2-52-50-101-139.eu-west-1.compute.amazonaws.com';
-conf.redis.port = 24249;
+//conf.redis.host = 'h:p4a0ccca5c529159eb6068ab5ea76bad8c04f67969ea86d3bc93c355d01abb34d@ec2-52-50-101-139.eu-west-1.compute.amazonaws.com';
+//conf.redis.port = 24249;
 
 var server = new Server().startup( conf );
 var casino = new Casino().startup( conf );
