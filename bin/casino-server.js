fb545a2e1e26452e9fa2003901f8794af60179bd
maybee <martin@bentzen.dk>
2020-03-31 00:18:34 +0200

    Update casino-server.js

--- bin/casino-server.js
+++ bin/casino-server.js
@@ -11,8 +11,10 @@ var argv = require('minimist')(process.argv.slice(2));
 if(argv.p) conf.server.port = argv.p;
 if(argv.h) conf.server.host = argv.h;
 
-conf.redis.host=url.parse(process.env.REDISCLOUD_URL).host; 
-conf.redis.port=url.parse(process.env.REDISCLOUD_URL).port; 
+conf.redis.host= 'redis-13992.c226.eu-west-1-3.ec2.cloud.redislabs.com';
+//url.parse(process.env.REDISCLOUD_URL).host; 
+conf.redis.port=13992;
+//url.parse(process.env.REDISCLOUD_URL).port; 
 conf.redis.passwd= '7Gdn1fP68wUEGVIin5kGapPzGrp6Ua5m';
 //url.parse(process.env.REDISCLOUD_URL).auth.split(“:”)[1]; 
 
