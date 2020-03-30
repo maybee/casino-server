3d17ccdb060751edf9474c7eb78f685299e35fac
maybee <martin@bentzen.dk>
2020-03-30 23:29:49 +0200

    Update casino-server.js

--- bin/casino-server.js
+++ bin/casino-server.js
@@ -10,21 +10,12 @@ var argv = require('minimist')(process.argv.slice(2));
 
 if(argv.p) conf.server.port = argv.p;
 if(argv.h) conf.server.host = argv.h;
-//if(argv.r) {
-//	var words = argv.r.split(':');
-//	if(words[0]) conf.redis.host = words[0];
-//	if(words[1]) conf.redis.port = parseInt(words[1]);
-//}
 
-if (process.env.REDISCLOUD_URL) {
-  // use production (Heroku) redis configuration
-  // overwrite config to keep it simple
-  var rtg = require(‘url’).parse(process.env.REDISCLOUD_URL);
-  conf.redis.port = rtg.port;
-  conf.redis.host = rtg.hostname;
-  conf.redis.password = rtg.auth.split(“:”)[1];
+if(argv.r) {
+	var words = argv.r.split(':');
+	if(words[0]) conf.redis.host = words[0];
+	if(words[1]) conf.redis.port = parseInt(words[1]);
 }
 
 var server = new Server().startup( conf );
 var casino = new Casino().startup( conf );
-
